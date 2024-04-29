74f75b66bb3a7c1407e08056ce67ed6265921035
RYAN GOYAL <109784570+ryangoyal007@users.noreply.github.com>
2024-04-29 15:06:02 +0530

    Add files via upload

---
+++ java/greater.java
@@ -0,0 +1,27 @@
+package greaternumber.java;
+
+public class Greaternumber {
+	int num1=40,num2=20;
+	void checkGreaternumber() {
+		if(num1>num2) {
+System.out.println("num1 is greater");
+}
+		else
+			
+		{
+			System.out.println("num2 is greater");
+		}
+	}
+	public static void main(String[] args) {
+	Greaternumber gn=new Greaternumber();
+	gn.num1=30;
+	gn.num2=20;
+	gn.checkGreaternumber();
+
+	}
+
+}
+//	 class Main{
+//
+//
+//}
